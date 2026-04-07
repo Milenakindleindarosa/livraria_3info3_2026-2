a2a5ead826b9d9c942460b7907ce2e2eff461d29
Milenakindleindarosa <milena.kindlein.da.da.rosa.ifc@gmail.com>
2026-04-07 09:17:13 -0300

    feat: múltiplos serializadores para Livro

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,6 +1,7 @@
 from .autor import AutorSerializer
 from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
-from .livro import LivroListRetrieveSerializer
+from .livro import LivroListSerializer
+from .livro import LivroRetrieveSerializer
 from .livro import LivroSerializer
 from .user import UserRegistrationSerializer, UserSerializer
