a2838188c0a92a8a95449dea1e165a028993b921
Milenakindleindarosa <milena.kindlein.da.da.rosa.ifc@gmail.com>
2026-04-07 09:07:10 -0300

    feat: dois serializadores para livro

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,5 +1,6 @@
 from .autor import AutorSerializer
 from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
+from .livro import LivroListRetrieveSerializer
 from .livro import LivroSerializer
 from .user import UserRegistrationSerializer, UserSerializer
--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -3,7 +3,14 @@ from rest_framework.serializers import ModelSerializer
 from core.models import Livro
 
 
+class LivroListRetrieveSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = '__all__'
+
+
 class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = '__all__'
+        deph = 1
